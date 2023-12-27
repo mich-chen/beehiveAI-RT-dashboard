12f8d433e5eb45248202b6d83cdcb03457c07db3
Michelle Chen <mich.chen.94@gmail.com>
2023-12-27 14:27:55 -0800

    add simple websocket logic and broadcasting

--- main.go
+++ main.go
@@ -2,8 +2,77 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// have a server to track all connections for broadcasting
+type Server struct {
+	conns map[*websocket.Conn]*websocket.Conn
+}
+
+// initiate a new Server
+func newServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]*websocket.Conn),
+	}
+}
+
+func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil) // upgrade the connection from HTTP to websocket to create new connection
+	if err != nil {
+		log.Println("Upgrade err:", err)
+	}
+	log.Println("Connection Successful!")
+	log.Println("New incoming connection from client: ", conn.RemoteAddr())
+
+	// add new connection to Server struct
+	s.conns[conn] = conn
+
+	defer conn.Close()
+
+	// read message loop
+	s.readLoop(conn)
+}
+
+func (s *Server) readLoop(conn *websocket.Conn) {
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read message err:", err)
+			break
+		}
+
+		// log message
+		log.Printf("Received message: %s", message)
+
+		// write messages and broadcast
+		s.broadcast(message)
+	}
+}
+
+func (s *Server) broadcast(message []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if err := ws.WriteMessage(websocket.TextMessage, message); err != nil {
+				fmt.Println("Broadcasting write message err:", err)
+			}
+		}(ws)
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	server := newServer()
+	http.HandleFunc("/websocket", server.handleWebsocket)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
