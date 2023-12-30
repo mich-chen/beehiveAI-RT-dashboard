899dd8691155aabbe89006e496ecaf81332ff4df
Michelle Chen <mich.chen.94@gmail.com>
2023-12-30 01:12:00 -0800

    styled <TotalResponses />

--- web/src/components/TotalResponses.tsx
+++ web/src/components/TotalResponses.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
 
 const TotalResponses: React.FC<{ total: number }> = ({ total }) => {
-  const header = "Total Tweets Received"
+  const header = "Total Tweets"
   return (
     <div className="metric">
-      <h4>{header}</h4>
-      <div>{total}</div>
+      <Card variant="outlined" sx={{ backgroundColor: "#90caf9" }}>
+        <CardHeader title={header} />
+        <CardContent className="widget">
+          <Typography variant="h3">
+            {total}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
