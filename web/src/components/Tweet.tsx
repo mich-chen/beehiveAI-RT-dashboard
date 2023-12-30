d6daba19c91b0e7caa26f8276eef05d67168a8ad
Michelle Chen <mich.chen.94@gmail.com>
2023-12-30 01:06:08 -0800

    styled <Tweet />

--- web/src/components/Tweet.tsx
+++ web/src/components/Tweet.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
 
 const Tweet: React.FC<{ name: string; text: string }> = ({ name, text }) => {
+  const userName = '@'.concat(name);
   return (
-    <div className="tweet">
-      <h4>{name}</h4>
-      <p>{text}</p>
-    </div>
+    <Card variant="outlined" sx={{ margin: 2 }}>
+      <CardHeader title={userName} sx={{ backgroundColor: "#42a5f5" }}></CardHeader>
+      <CardContent className="tweet">
+        <Typography variant="body1">
+          {text}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
